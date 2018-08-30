02b32809fd11ecadc52273cff08f92057192921f
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-30 20:59:52 +1000

    add newjs

--- db/migrate/20180419022335_create_generic_checklist_statuses.rb
+++ db/migrate/20180419022335_create_generic_checklist_statuses.rb
@@ -1,7 +1,7 @@
 class CreateGenericChecklistStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :generic_checklist_statuses do |t|
-      t.references :generic_check_list, foreign_key: true
+      t.references :generic_check_list, index: true
       t.references :user, foreign_key: true
       t.boolean :completed
 
