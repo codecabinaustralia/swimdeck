d3321b17ab1252577f1b4b9cb587a853332554a8
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-06 17:24:03 +1000

    add data

--- app/controllers/data_scraper_controller.rb
+++ app/controllers/data_scraper_controller.rb
@@ -315,7 +315,7 @@ redirect_to root_path
 	  	@new_date = Date.parse(link.StuBookStartDate).next_occurring(@myday)
 	  	puts @new_date
 
-		@lesson_start = @new_date + " " + @lesson_start_time.strftime("%I:%M:00")
+		@lesson_start = @new_date.to_date + " " + @lesson_start_time.strftime("%I:%M:00")
 
 	  	@time_reformat = @lesson_start.to_date.strftime("%Y-%m-%d %I:%M%p")
 	  	@lesson_finish = @time_reformat
