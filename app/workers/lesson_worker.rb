7f3db3cece2f3bc5b23370d462e883f5903a4e12
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-10 22:10:55 +1000

    New Worker to Lesson

--- app/workers/lesson_worker.rb
+++ app/workers/lesson_worker.rb
@@ -5,8 +5,7 @@ class LessonWorker
 
   def perform()
   	
-	require 'date'
-  	require 'time'
+
   	#Find all links
   	@links = Link.all
 
@@ -107,13 +106,52 @@ class LessonWorker
 
 		#The student needs a parent let's create a client/parent
 		#first create the login / user
-	
+		parent = User.find_or_create_by(email: "#{link.RPEmail}") do |p|
+					p.password = "Test123"
+					p.password_confirmation = "Test123"
+					p.current_sign_in_at = DateTime.now
+					p.last_sign_in_at = DateTime.now
+					p.created_at = DateTime.now
+					p.admin = false
+					p.manager = false
+					p.pool_deck_leader = false
+					p.teacher = false
+					p.customer_service = false
+					p.client = true
+					p.first_name = link.RPGivenNames
+					p.last_name = link.RPSurname
+				  end
 
+		#Now the client details record
+		client = Client.find_or_create_by(email: "#{link.RPEmail}") do |c|
+			c.user_id = c_user.id
+			c.first_name = link.RPGivenNames
+			c.last_name = link.RPSurname
+			c.phone_1 = link.RPPhone
+			c.phone_2 = link.RPWorkPhone
+			c.address = link.RPAddress
+			c.address_city = link.RPSuburb
+			c.address_state = "QLD"
+			c.address_postcode = link.RPPostCode
+		end
+
+
+		#Let's connect the student to the client
+		student_client = ClientStudent.find_or_create_by(student_id: student.id, client_id: client.id)
 
 		# Finally we can add the skills to the students
 		# Currently we are just adding the skills from the @current_level and assigning them as incompetant
 			
-	    
+	    @students = Student.all
+	    @students.each do |student|
+		    @skills = Skill.where(level_id: student.current_level).all
+		    @skills.each do |skill|
+			    StudentSkill.find_or_create_by(student_id: student.id, skill_id: skill.id) do |sk|
+					sk.level_id = student.current_level,
+					sk.competency_level_id = 1
+			    end
+			end
+	  	end
   	end
 
 end
