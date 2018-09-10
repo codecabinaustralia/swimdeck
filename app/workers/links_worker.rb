f44e2c5e18f486348cb05dc6e57f4cdd416c6999
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-10 15:20:34 +1000

    newworker

--- app/workers/links_worker.rb
+++ app/workers/links_worker.rb
@@ -21,9 +21,9 @@ class LinksWorker
 	csv = CSV.parse(csv_text, :headers=>true)
 
 	csv.each do |row|
-		data = row[0].split("|")
-  		    @today_date = data.values_at(9).join('').to_date.strftime("%Y-%m-%d")
-          if (@today_date < (Date.today-7.days))
+		data = row[0].split("|") 
+  		    @today_date = DateTime.parse("data.values_at(9).join('') 00:00:00").strftime("%Y-%m-%d")
+          if (@today_date < (Date.today-7.days).strftime("%Y-%m-%d"))
   	  		Link.create(
   		  		StuSurname: data.values_at(0).join(''),
   		  		StuGivenNames: data.values_at(1).join(''),
