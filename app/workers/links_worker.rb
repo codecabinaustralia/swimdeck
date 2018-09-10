036f895130c5290062f2339aa494646705d0d49b
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-10 16:07:18 +1000

    add git

--- app/workers/links_worker.rb
+++ app/workers/links_worker.rb
@@ -23,9 +23,9 @@ class LinksWorker
 	csv.each do |row|
 		data = row[0].split("|") 
   		    
-          
+
           @csv_date = Date.strptime("data.values_at(9).join('')", "%d-%b-%y").strftime("%d-%b-%y")
-          if (@csv_date >= (Date.today).strftime("%d-%b-%y"))
+          if (@csv_date <= (Date.today).strftime("%d-%b-%y"))
   	  		Link.create(
   		  		StuSurname: data.values_at(0).join(''),
   		  		StuGivenNames: data.values_at(1).join(''),
