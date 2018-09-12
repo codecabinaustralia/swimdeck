dfcff255312dca3f8226de90951f90e60afe1dc3
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-13 05:36:19 +1000

    readdworker

--- app/workers/links_worker.rb
+++ app/workers/links_worker.rb
@@ -23,37 +23,36 @@ class LinksWorker
 	csv.each do |row|
 		data = row[0].split("|")
   		    
-  	  		new_link = Link.new(
-  		  		StuSurname: data.values_at(0).tr_s('"', '').strip.join('')
-  		  		StuGivenNames: data.values_at(1).tr_s('"', '').strip.join('')
-  		  		StudId: data.values_at(2).tr_s('"', '').strip.join('')
-  		  		LessonDay: data.values_at(3).tr_s('"', '').strip.join('')
-  		  		LessonTime: data.values_at(4).tr_s('"', '').strip.join('')
-  		  		LessonLevel: data.values_at(5).tr_s('"', '').strip.join('')
-  		  		Area: data.values_at(6).tr_s('"', '').strip.join('')
-  		  		TeachSurname: data.values_at(7).tr_s('"', '').strip.join('')
-  		  		TeachGivenNames: data.values_at(8).tr_s('"', '').strip.join('')
-  		  		StuBookStartDate: data.values_at(9).tr_s('"', '').strip.join('')
-  		  		RPSurname: data.values_at(10).tr_s('"', '').strip.join('')
-  		  		RPGivenNames: data.values_at(11).tr_s('"', '').strip.join('')
-  		  		RPId: data.values_at(12).tr_s('"', '').strip.join('')
-  		  		RPAddress: data.values_at(13).tr_s('"', '').strip.join('')
-  		  		RPSuburb: data.values_at(14).tr_s('"', '').strip.join('')
-  		  		RPPostCode: data.values_at(15).tr_s('"', '').strip.join('')
-  		  		RPHomePhone: data.values_at(16).tr_s('"', '').strip.join('')
-  		  		RPWorkPhone: data.values_at(17).tr_s('"', '').strip.join('')
-  		  		RPPhone: data.values_at(18).tr_s('"', '').strip.join('')
-  		  		RPEmail: data.values_at(19).tr_s('"', '').strip.join('')
-  		  		RPBookingBalance: data.values_at(20).tr_s('"', '').strip.join('')
-  		  		RPBookingFee: data.values_at(21).tr_s('"', '').strip.join('')
-  		  		FamilyPaymentType: data.values_at(22).tr_s('"', '').strip.join('')
-  		  		SpecialDiscount: data.values_at(23).tr_s('"', '').strip.join('')
-  		  		RP: data.values_at(24).tr_s('"', '').strip.join('')
-  		  		StuGender: data.values_at(25).tr_s('"', '').strip.join('')
-  		  		StuDateOfBirth: data.values_at(26).tr_s('"', '').strip.join('')
+  	  		Link.create(
+  		  		StuSurname: data.values_at(0).join(''),
+  		  		StuGivenNames: data.values_at(1).join(''),
+  		  		StudId: data.values_at(2).join(''),
+  		  		LessonDay: data.values_at(3).join(''),
+  		  		LessonTime: data.values_at(4).join(''),
+  		  		LessonLevel: data.values_at(5).join(''),
+  		  		Area: data.values_at(6).join(''),
+  		  		TeachSurname: data.values_at(7).join(''),
+  		  		TeachGivenNames: data.values_at(8).join(''),
+  		  		StuBookStartDate: data.values_at(9).join(''),
+  		  		RPSurname: data.values_at(10).join(''),
+  		  		RPGivenNames: data.values_at(11).join(''),
+  		  		RPId: data.values_at(12).join(''),
+  		  		RPAddress: data.values_at(13).join(''),
+  		  		RPSuburb: data.values_at(14).join(''),
+  		  		RPPostCode: data.values_at(15).join(''),
+  		  		RPHomePhone: data.values_at(16).join(''),
+  		  		RPWorkPhone: data.values_at(17).join(''),
+  		  		RPPhone: data.values_at(18).join(''),
+  		  		RPEmail: data.values_at(19).join(''),
+  		  		RPBookingBalance: data.values_at(20).join(''),
+  		  		RPBookingFee: data.values_at(21).join(''),
+  		  		FamilyPaymentType: data.values_at(22).join(''),
+  		  		SpecialDiscount: data.values_at(23).join(''),
+  		  		RP: data.values_at(24).join(''),
+  		  		StuGender: data.values_at(25).join(''),
+  		  		StuDateOfBirth: data.values_at(26).join(''),
   		  		StuAge: data.values_at(27).join('')
   	  		)
-        new_link.save
         
   	end
 
