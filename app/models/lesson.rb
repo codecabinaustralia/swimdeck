3d9156b1f0b86825b7435acf7da5a0ce371f5237
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-24 14:10:49 +1000

    fix achievers 2

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,8 +6,7 @@ class Lesson < ApplicationRecord
   require 'time'
 
  def custom_method
-  date_time_new = DateTime.strptime("01/01/2000 " + lesson_time, '%d/%m/%Y %-I:%M%p')
-  Date.new(date_time_new).strftime('%I%M%p')
+ 	Time.parse("2011-04-12 #{lesson_time}")
  end
 
 
